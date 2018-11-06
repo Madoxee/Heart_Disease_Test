df38e1b545009bba4ca6f283c71e75bc666e4840
Madoxee <mamodo1994@gmail.com>
2018-11-06 09:18:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+//vars 
+var P_C = 92/209;
+var _P_C = 117/209;
+var P_Pain_Sick = 61/92 ;
+var P_Pain_NotSic = 12/117 ;
+var P_NoPain_Sick = 31/92;
+var P_NoPain_NotSick = 105/117;
+var P_rest_electro_normal_Sick = 73/92 ;
+var P_rest_electro_normal_NotSick = 96/117;
+var P_rest_electro_st_t_wave_abnormality_Sick = 18/92 ;
+var P_rest_electro_st_t_wave_abnormality_NotSick= 17/117;
+var P_rest_electro_left_vent_hyper_Sick = 1/92 ;
+var P_rest_electro_left_vent_hyper_NotSick= 4/117;
+var P_blood_sugar_True_Sick = 11/92;
+var P_blood_sugar_True_NotSick= 4/117;
+var P_blood_sugar_Fasle_Sick = 81/92;
+var P_blood_sugar_Fasle_NotSick= 113/117;
+var P_chest_pain_type_asympt_Sick = 75/92;
+var P_chest_pain_type_asympt_NotSick= 34/117;
+var P_atyp_angina = 65/206;
+var P_chest_pain_type_atyp_angina_Sick = 6/92;    
+var P_chest_pain_type_atyp_angina_NotSick= 53/117;
+var P_chest_pain_type_non_anginal_Sick = 7/92;
+var P_chest_pain_type_non_anginal_NotSick= 29/117;
+var P_chest_pain_type_typ_angina_Sick = 4/92;
+var P_chest_pain_type_typ_angina_NotSick= 1/117;
+
